8e0bab16eeca4002e07f214c030c00520f06cf94
fsGe77hU3giMfse <FilipWHolming@gmail.com>
2023-09-25 16:28:00 +0200

    ?

---
+++ FileDiffCompare/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FileDiffCompare
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<TabItemViewModel> Tabs { get; set; }
+
+        public ICommand NewTabCommand { get; set; }
+        public ICommand AboutCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Tabs = new ObservableCollection<TabItemViewModel>();
+            NewTabCommand = new RelayCommand(AddNewTab);
+            AboutCommand = new RelayCommand(DisplayAbout);
+        }
+
+        private void AddNewTab(object _)
+        {
+            var newTab = new TabItemViewModel
+            {
+                Title = $"Tab {Tabs.Count + 1}"
+                //... set other properties as needed 
+            };
+            Tabs.Add(newTab);
+        }
+
+        private void DisplayAbout(object _)
+        {
+            MessageBox.Show("FileDiffCompare - Created by YourName. Version 1.0.0");
+        }
+
+        private string _someProperty;
+        public string SomeProperty
+        {
+            get { return _someProperty; }
+            set
+            {
+                if (_someProperty != value)
+                {
+                    _someProperty = value;
+                    OnPropertyChanged(nameof(SomeProperty));
+                }
+            }
+        }
+
+        // ... other properties and methods ...
+    }
+}
---
+++ FileDiffCompare/ViewModels/TabItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace FileDiffCompare
+{
+    public class TabItemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Example property
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        // ... other properties and methods for your TabItemViewModel ...
+    }
+}
